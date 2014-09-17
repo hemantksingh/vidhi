0bfeaf8d422a7e15eaf880704ce9227d60c927e5
Hemant Kumar <hemantksingh.hk@gmail.com>
2014-09-17 17:40:30 +0100

    Enabled sign up using angular to be a SPA.

--- public/src/app.js
+++ public/src/app.js
@@ -6,11 +6,17 @@ angular.module('vidhi', ['ngResource', 'ngRoute'])
 		templateUrl: '/partials/sign-in.html', 
 		controller : 'signInController'
 	})
+	.when('/sign-up', {
+		templateUrl: '/partials/sign-up.html', 
+		controller : 'signUpController'
+	})
 	.otherwise({
 		redirectTo: '/'
 	});
 })
 .controller('signInController', function($scope) {
 	$scope.title = "Vidhi - Sign in";
+})
+.controller('signUpController', function($scope) {
+	$scope.title = "Vidhi - Sign up";
 });
-
