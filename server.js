1b0902ad9ec4b2a59f5d172356531cd2a2e5fdac
Hemant Kumar <hemantksingh.hk@gmail.com>
2014-09-19 02:35:08 +0100

    Seeded the database with users.

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 var env = process.env.NODE_ENV = 
-		process.env.NODE_ENV || 'development';
+process.env.NODE_ENV || 'development';
 var config = require('./server/config/config')[env];
 var app = require('./server/config/express')(config);
 var database = require('./server/database')(config);
 var routes = require('./server/config/routes')(app);
+var seedData = require('./server/seedData');
 
 app.listen(config.port, function() {
-		console.log("Vidhi is running at localhost:" + config.port);
-	});
+	console.log("Vidhi is running at localhost:" + config.port);
+});
 
 database.getDb(function(err, theDb) {
 	if(theDb) {
 		console.log("Connected to the database:- " + theDb.db.options.url);
+		theDb.users.count(function(err, count) {
+			if(err) {
+				console.log("Failed to get the users count.");
+			} else if(count === 0) {
+				seedData.users.forEach(function(user) {
+					theDb.users.insert(user, function(err) {
+						if(err) {
+							console.log("Failed to seed database");	
+						}
+					});
+				});
+			} else {
+				console.log("Database already seeded.");
+			}
+		});
 	} else if(err) {
 		console.log("Failed to connect to the database.");
 	}
---
+++ server/seedData.js
@@ -0,0 +1,21 @@
+module.exports = {
+	users :
+		[{
+			firstName: "Vidhi",
+			lastName: "Vidhi",
+			email: "vidhi@gmail.com",
+			firmName: "iNivaran"	
+		},
+		{
+			firstName: "Hemant",
+			lastName: "Kumar",
+			email: "hemantksingh.hk@gmail.com",
+			firmName: "iNivaran"
+		},
+		{
+			firstName: "Adam",
+			lastName: "Piper",
+			email: "adam@gmail.com",
+			firmName: "iNivaran"	
+		}]
+};
