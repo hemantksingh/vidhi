df464b9518377a44b288e3c18218a11411da52b2
Hemant Kumar <hemantksingh.hk@gmail.com>
2014-09-27 16:11:52 +0100

    Changed the deserialization to use userRepository for retreiving user.

--- server/startUp/passport.js
+++ server/startUp/passport.js
@@ -1,31 +1,27 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-module.exports = function(authorization, database) {
+module.exports = function(authorization, userRepository) {
 	passport.use(new LocalStrategy(authorization.verifyUser));
 	passport.serializeUser(function(user, callback) {
-		console.log("Serializing.. " + JSON.stringify(user));
 		if(user) {
-			callback(null, user.email);
+			console.log("Serializing user... " + user.username);
+			callback(null, user.username);
+		} else {
+			console.log("Unable to serialize as no user was found.");
 		}
 	});
 
 	passport.deserializeUser(function(key, callback) {
-		database.getDb(function(err, db){
-			if(err) {
-				callback(null, false, {message: "Failed to retrieve user"});
-			} else {  
-				console.log("Finding user for deserialization..");
-				db.users.findOne({email: key}, function(err, user){
-					if(!err && user) {
-						callback(null, user);
-					} else if(err){
-						callback(null, false, {message: "Failed to retrieve user"});
-					} else {
-						callback(null, false, {message: "No user found"});
-					}
-				});
+		userRepository.getUser(key, function(err, user){
+			if(!err && user) {
+				console.log("User retrieved for deserialization..");
+				callback(null, user);
+				return;
 			}
+
+			console.log("Failed to retrieve user for deserialization.");
+			callback(null, false, "Failed to retrieve user for deserialization.");
 		});
 	});
 };
