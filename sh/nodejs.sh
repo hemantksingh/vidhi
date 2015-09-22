3a6d6a1c8356073902e76256986eefcc791871be
hemantksingh <hemantksingh.hk@gmail.com>
2015-09-22 12:39:54 +0000

    Ensures grunt & bower are installed on the vagrant box.

--- sh/nodejs.sh
+++ sh/nodejs.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo "--- Installing node.js ---"
 # Modified from https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
 apt-get update
 apt-get install -y python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
+
+echo "--- Installing grunt-cli ---"
+npm install -g grunt-cli
+
+echo "--- Installing Bower ---"
+npm install -g bower
